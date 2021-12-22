5f2cf38268e5210d76a0d9b1ca613c034fcff688
jungho shin <wjdghtls11@gmail.com>
2021-12-23 00:19:09 +0900

    operators prac

---
+++ js-basic/operators.js
@@ -0,0 +1,23 @@
+let apples = '2';
+let oranges = '3';
+
+console.log(+apples + +oranges);
+
+// #1
+
+let a = 1, b = 1;
+
+let c = ++a;
+let d = b++;
+
+console.log('c:', c, 'd:', d);
+
+// #2
+
+let a = 2;
+let x = 1 + (a *= 2); 
+
+// a = 4 ,x = 5
+
+// #3
+
