60127cb0a004fda1373697e3d7da33dac3c5085f
jungho shin <wjdghtls11@gmail.com>
2021-12-22 22:54:17 +0900

    types prac, nodemon download

---
+++ js-basic/types.js
@@ -0,0 +1,9 @@
+// let message = "hello";
+// message = 123456;
+console.log(1 / 0);
+
+let name = 'Ilya';
+
+console.log( `hello ${1}` );  // 1
+console.log( `hello ${'name'}` ); // name 
+console.log( `hello ${name}` ); // Ilya
