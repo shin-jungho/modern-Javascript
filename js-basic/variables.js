d35125e174404311391f7ea2713e3f8a34dd6fa0
jungho shin <wjdghtls11@gmail.com>
2021-12-22 22:28:08 +0900

    variables prac

---
+++ js-basic/variables.js
@@ -0,0 +1,16 @@
+// #1
+
+let name = "John";
+
+let admin = name;
+
+console.log(admin);
+
+// #2
+
+let ourPlanetName = 'earth';
+let currentUserName = 'John';
+
+
+// #3
+// 바뀌지 않으면 대문자 상수 적합 but age같은 경우는 매년 바뀌므로 적합 x
