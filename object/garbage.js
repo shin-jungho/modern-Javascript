4bb14730f069e82a8889af10d0f4f247200c2d26
jungho shin <wjdghtls11@gmail.com>
2022-01-01 17:20:38 +0900

    garbage collection Prac

---
+++ object/garbage.js
@@ -0,0 +1,17 @@
+function marry(man, woman) {
+  woman.husband = man;
+  man.wife = woman;
+
+  return { 
+    father: man,
+    mother: woman
+  }
+}
+
+let family = marry({
+  name: 'John'
+}, {
+  name:'Ann'
+});
+
+console.log(marry());
